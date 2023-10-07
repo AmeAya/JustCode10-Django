75fc69ca1e64310c1fadba672d1836348fbec48f
AmeAya <deobackstep@gmail.com>
2023-10-07 20:44:42 +0600

    html show datas

--- blogapp/views.py
+++ blogapp/views.py
@@ -5,4 +5,5 @@ from django.views.generic import ListView, CreateView, UpdateView, DetailView, D
 
 class PostListView(ListView):
     model = Post
+    context_object_name = 'posts'  # Под каким именем данные будут отзыватся в html
     template_name = 'post_list_template.html'
