479998ee5fd8a165b3a23b729749b7a5416f0396
AmeAya <deobackstep@gmail.com>
2023-10-07 20:21:49 +0600

    Added ListView, Templates, urls

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list_template.html'
