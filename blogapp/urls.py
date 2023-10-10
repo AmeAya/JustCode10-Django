f118b39ee4eb93d0fedf42ce535b63ac0fb354bb
AmeAya <deobackstep@gmail.com>
2023-10-10 19:31:03 +0600

    DetailView

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,4 +4,5 @@ from .views import *
 # path('<Роут маршрут>', <Имя вашей вьюшки>.as_view(), name='<Внутреннее имя для маршрута>'),
 urlpatterns = [
     path('posts', PostListView.as_view(), name='post_list_url'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail_url'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -7,3 +7,9 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'  # Под каким именем данные будут отзыватся в html
     template_name = 'post_list_template.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'post_detail_template.html'
